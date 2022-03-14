9c56e15d0fd0ae3a472de727ad32a9b23556ff68
jamesb97 <zdravko6t5@gmail.com>
2022-03-14 17:29:48 -0400

    first commit

---
+++ src/Recipe.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import style from './recipe.module.css';
+const Recipe = ({ title, calories, image, ingredients }) => {
+    return (
+        <div className={style.recipe}>
+            <h1>{title}</h1>
+            <h3>Ingredients:</h3>
+            <ul>{ingredients.map(ingredient => (<li>{ingredient.text}</li>))}</ul>
+            <p className={style.calories}>Calories:  {calories}</p>
+            <img className={style.image} src={image} alt="Food info" />
+        </div>
+    );
+}
+
+export default Recipe;
